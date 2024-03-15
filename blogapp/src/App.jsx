6d3f7def157650129ad1a9de76e281eb06f0ae62
kaurnavjeet <kaurnavjeet09@gmail.com>
2024-03-15 16:07:43 -0700

    completed pages

--- blogapp/src/App.jsx
+++ blogapp/src/App.jsx
@@ -1,17 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import "./App.css";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [loading, setLoading] = useState(true);
+	const dispatch = useDispatch();
 
-	return (
-		<div>
-			<main>
-				<Outlet />
-			</main>
+	useEffect(() => {
+		authService
+			.getCurrentUser()
+			.then((userData) => {
+				if (userData) {
+					dispatch(login({ userData }));
+				} else dispatch(logout());
+			})
+			.finally(() => setLoading(false));
+	}, [dispatch]);
+
+	return !loading ? (
+		<div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+			<div className="w-full block">
+				<Header />
+				<main>
+					<Outlet />
+				</main>
+			</div>
+			<div className="w-full block">
+				<Footer />
+			</div>
 		</div>
-	);
+	) : null;
 }
 
 export default App;
